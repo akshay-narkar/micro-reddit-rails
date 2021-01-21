c940d73de6bdc9fbaa7fcfab671c39e43b436c8d
dibdas <dibyandu08das@gmail.com>
2021-01-21 13:49:50 +0530

    updated post.eb updated user.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   validates :body, presence: true, length: { maximum: 200 }
+  validates :title, presence: true, length: { maximum: 20 }
 end
